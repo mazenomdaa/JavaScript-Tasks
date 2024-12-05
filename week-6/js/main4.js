13565fc81477c0932be230774dcf091f06c209ab
Mazen Omda <mazen0omda@gmail.com>
2024-12-05 06:32:42 +0200

    week-6 GLI-111
    assignment 4  main.js edited

--- week-6/js/main4.js
+++ week-6/js/main4.js
@@ -1,4 +1,4 @@
 let website = "Go";
 let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
 
-console.log(words[true+true][0].slice(true+true).toUpperCase()); // ZERO
+console.log(words[true + true][0].slice(website.length).toUpperCase()); // ZERO
