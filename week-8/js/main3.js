7fc245ba3187eadf17396f8f509affe1f6b094f5
Mazen Omda <mazen0omda@gmail.com>
2024-12-11 02:09:52 +0200

    week-8 GLI-125
    Question-3 updated.

--- week-8/js/main3.js
+++ week-8/js/main3.js
@@ -1,10 +1,21 @@
 function ageInTime(theAge) {
   // Your Code Here
-  if (theAge > 80) {
+  if (theAge <= 10 || theAge >= 100) {
     return `Age Out Of Range`;
-  } else {
-    return theAge * 12;
   }
+  months = theAge * 12;
+  weeks = theAge * 52;
+  days = theAge * 365;
+  hours = days * 24;
+  minutes = hours * 60;
+  seconds = minutes * 60;
+
+  console.log(`${months} Months`);
+  console.log(`${weeks} Weeks`);
+  console.log(`${days} Days`);
+  console.log(`${hours} Hours`);
+  console.log(`${minutes} Minutes`);
+  console.log(`${seconds} Seconds`);
 }
 
 // Needed Output
